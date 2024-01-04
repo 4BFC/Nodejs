2513fbd95129dbd5e1e817219b7cccf97f458ebd
4BFC <4bee.code@gmail.com>
2024-01-04 17:29:17 +0900

    edited files

--- test/lib/template.js
+++ test/lib/template.js
@@ -5,32 +5,33 @@
 //}
 module.exports = {
 
-  list: function () {//queryData_id
-    // let main_tag = '<a href="/">main page</a>';
-
-    let list_page = '';
+  list: function (queryData_id) {//queryData_id
+    let main_page = '<a href="/">main page</a><br>';
+    // let change_page = ''
+    let list_page = [];
+    //각 페이지 생성하는 코드
     for (let i = 0; i < 4; i++) {
-      list_page += `<a href="/?id = ${i + 1}_page">${i + 1} page</a><br>`;
+      list_page.push(`<a href="/?id=${i + 1}_page">${i + 1} page</a><br>`);
+      if (queryData_id === `${i + 1}_page`) {
+        list_page[i] = main_page;
+      }
     }
+    let list_pageString = list_page.join(''); //문자열로 처리하지 않으면 undefined가 된다.
+    //해당 페이지(queryData.id)가 선택될 경우 해당 자리는 main_page로 작성
+    // console.log(list_page)
 
-    console.log(list_page)
-    // if (queryData_id) {
-
-    // }
-    let main_tag = '<a href="/">main page</a>';
+    // let main_tag = '<a href="/">main page</a>';
     return `
       <!DOCTYPE html>
         <html lang="en">
         <head>
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>2nd_page</title>
+          <title>${queryData_id}</title>
         </head>
         <body>
-        <h1>This is 2nd_page</h1>
-        ${main_tag}
-        <br>
-        ${list_page}
+        <h1>This is ${queryData_id}</h1>
+        ${list_pageString}
         </body>
       </html>
       `
--- test/main.js
+++ test/main.js
@@ -24,45 +24,30 @@ let app = http.createServer((request, response) => {
         </head>
         <body>
         <h1>This is main page</h1>
-        <a href="/?id=1st_page">1st page</a>
-        <a href="/?id=2nd_page">2nd page</a>
-        <a href="/?id=3th_page">3th page</a>
-        <a href="/?id=4th_page">4th page</a>
-        </body>
-      </html>
-      `
-      response.writeHead(200);
-      response.end(HTML);
-    } else {
-      let HTML = `
-      <!DOCTYPE html>
-        <html lang="en">
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>${queryData.id}</title>
-        </head>
-        <body>
-        <h1>This is ${queryData.id}</h1>
         <a href="/?id=1_page">1 page</a>
         <a href="/?id=2_page">2 page</a>
         <a href="/?id=3_page">3 page</a>
         <a href="/?id=4_page">4 page</a>
-        <a href="/other">other page</a>
         </body>
       </html>
       `
+      console.log(queryData.id)
+      response.writeHead(200);
+      response.end(HTML);
+    } else {
+      let HTML = list.list(queryData.id);
+      console.log(queryData.id)
       response.writeHead(200);
       response.end(HTML);
     }
   }//if(pathname === '/')
-  else if (pathname === '/other') {
+  // else if (pathname === '/other') {
 
-    let HTML = list.list();
+  //   let HTML = list.list(queryData.id);
 
-    response.writeHead(200);
-    response.end(HTML);
-  }
+  //   response.writeHead(200);
+  //   response.end(HTML);
+  // }
 });
 //서버 리스닝
 app.listen(3000);
