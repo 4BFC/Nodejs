75c47f3dd496acca5ac862c321d531ce211c8ab4
4Bee <4bee.code@gmail.com>
2024-06-23 23:50:52 +0900

    📣 : section28 강의를 복습하면서 불필요한 파일들 삭제 및 동기와 비동기를 공부했다.

--- lecture/생활코딩/section28_2/syntax/sync.js
+++ lecture/생활코딩/section28_2/syntax/sync.js
@@ -2,7 +2,32 @@ let fs = require('fs');
 
 console.log('A');
 // let result = fs.readFileSync('syntax/sample.txt', 'utf8');//동기적 방식
-fs.readFile('syntax/sample.txt', 'utf8', function (err, result) {
-  console.log(result)
-});//비동기적 방식
-console.log('C')
+/*
+* 비동기 방식
+fs.readFile('sample.txt', 'utf8', function (err, result) {
+  console.log(result);
+});*/
+
+// Promise() then절을 사용해서 출력하기
+/**
+const fs = require('fs');
+
+function readFilePromise(filePath, encoding) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, encoding, (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
+}
+
+// 이제 프로미스 기반으로 파일을 읽어보겠습니다.
+readFilePromise('sample.txt', 'utf8')
+  .then(data => {
+    console.log(data);  // 파일의 내용을 출력
+  })
+  .catch(err => {
+    console.error('Error reading file:', err);
+  });
+
+ */
