251a427ac461b854277deaa99d8f9cc66d7d776f
4Bee <4bee.code@gmail.com>
2024-06-24 00:14:34 +0900

    📣: section28(1~3) 강의를 복습하면서 불필요한 파일 제거 및 callback함수의 개념을 상기했다.

--- lecture/생활코딩/section28_2/syntax/sync.js
+++ lecture/생활코딩/section28_2/syntax/sync.js
@@ -2,11 +2,11 @@ let fs = require('fs');
 
 console.log('A');
 // let result = fs.readFileSync('syntax/sample.txt', 'utf8');//동기적 방식
-/*
-* 비동기 방식
+//비동기적 방식
 fs.readFile('sample.txt', 'utf8', function (err, result) {
   console.log(result);
-});*/
+});
+console.log('C');
 
 // Promise() then절을 사용해서 출력하기
 /**
